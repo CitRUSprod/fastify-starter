63ac02d9124043d0fac48f044f77aa2db4ffff6a
CitRUS <citrus4009@gmail.com>
2022-05-01 18:32:25 +0300

    feat: Improve ajv errors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import cookie from "fastify-cookie"
 import auth from "fastify-auth"
 import Ajv, { ErrorObject } from "ajv"
 import ajvKeywords from "ajv-keywords"
+import { AggregateAjvError } from "@segment/ajv-human-errors"
 import { getEnv } from "$/utils"
 import { decorators } from "$/decorators"
 import { routes } from "$/routes"
@@ -24,12 +25,10 @@ const port = 6500
 const app = fastify()
 
 app.setValidatorCompiler(({ schema }) => ajv.compile(schema) as any)
-app.setSchemaErrorFormatter(errors => {
-    const err = errors[0] as any as ErrorObject
-    const varName = err.instancePath.slice(1)
-    const msg = err.message!
-
-    return new Error(`${varName} ${msg}`)
+app.setSchemaErrorFormatter((errors, dataVar) => {
+    const humanErrors = new AggregateAjvError(errors as any) as any
+    const err = humanErrors.errors[0] as ErrorObject
+    return new Error(`[${dataVar}] ${err.message!}`)
 })
 
 if (env.ENABLE_DOCS) {
